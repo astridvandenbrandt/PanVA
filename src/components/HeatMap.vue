7fc76780a4b7ef2b6161c43a946769b52bceff12
astridvandenbrandt <astridvandenbrandt@gmail.com>
2022-08-08 18:29:14 +0200

    error 1031 ipv 1030

--- src/components/HeatMap.vue
+++ src/components/HeatMap.vue
@@ -6714,7 +6714,7 @@ export default {
       // //// console.log('selected id fror post', selected_id)
 
       let res = await axios.post(
-        `http://131.155.23.192:1031/${selected_id}/d3dendro`,
+        `http://131.155.23.192:1030/${selected_id}/d3dendro`,
         { selected_items: selected }
       ) // this should be moved to the store as well ?
       // //// console.log('res.data', res.data)
